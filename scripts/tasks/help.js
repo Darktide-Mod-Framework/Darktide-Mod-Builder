7d3e0290052af8f5a3001fb2ac1fe918105e219b
Ashe <leicasimile@gmail.com>
2025-01-07 22:28:25 -0500

    Update references from vmb to dmb in help command (#2)

--- scripts/tasks/help.js
+++ scripts/tasks/help.js
@@ -37,39 +37,39 @@ module.exports = async function taskHelp(taskName) {
         console.log(
 
             `Usage:\n` +
-            `  vmb <command> [command-specific params] [-f <mods_folder>] [-g {1|2}] [--cfg <path_to_item_cfg>] [--dot]\n` +
+            `  dmb <command> [command-specific params] [-f <mods_folder>] [-g {1|2}] [--cfg <path_to_item_cfg>] [--dot]\n` +
             `                                          [--rc <config_folder>] [--reset] [--use-fallback] [--cwd] [--debug]\n\n` +
 
             `Commands:\n` +
-            '  vmb config  [--<key1>=<value1> --<key2>=<value2>...]\n\n' +
+            '  dmb config  [--<key1>=<value1> --<key2>=<value2>...]\n\n' +
 
-            '  vmb create  <mod_name> [-d <description>] [-t <title>] [-l <language>] [-v {private|public|friends}]\n' +
+            '  dmb create  <mod_name> [-d <description>] [-t <title>] [-l <language>] [-v {private|public|friends}]\n' +
             '                         [-c <content_folder>] [--tags "<tag1>; <tag2>;..."] [--template <template_folder>]\n\n' +
 
-            '  vmb publish <mod_name> [-d <description>] [-t <title>] [-l <language>] [-v {private|public|friends}]\n' +
+            '  dmb publish <mod_name> [-d <description>] [-t <title>] [-l <language>] [-v {private|public|friends}]\n' +
             '                         [-c <content_folder>] [--tags "<tag1>; <tag2>;..."]\n' +
             '                         [--ignore-errors] [--verbose] [--clean] [--source]\n\n' +
 
-            '  vmb upload  {<mod_name1> <mod_name2>... | --all}  [-n <changenote>] [--open] [--skip]\n\n' +
+            '  dmb upload  {<mod_name1> <mod_name2>... | --all}  [-n <changenote>] [--open] [--skip]\n\n' +
 
-            '  vmb open    {<mod_name> | --id <item_id>}\n\n' +
+            '  dmb open    {<mod_name> | --id <item_id>}\n\n' +
 
-            '  vmb build   [<mod_name1> <mod_name2>...] [--ignore-errors] [--verbose] [--clean] [--id <item_id>]\n' +
+            '  dmb build   [<mod_name1> <mod_name2>...] [--ignore-errors] [--verbose] [--clean] [--id <item_id>]\n' +
             '                                           [--no-workshop] [--source]\n\n' +
 
-            '  vmb watch   [<mod_name1> <mod_name2>...] [--ignore-errors] [--verbose] [--clean] [--id <item_id>]\n' +
+            '  dmb watch   [<mod_name1> <mod_name2>...] [--ignore-errors] [--verbose] [--clean] [--id <item_id>]\n' +
             '                                           [--no-workshop] [--source]\n\n' +
 
-            '  vmb info    [<mod_name1> <mod_name2>...] [--show-cfg]\n\n' +
+            '  dmb info    [<mod_name1> <mod_name2>...] [--show-cfg]\n\n' +
 
             'For more information on specific commands:\n' +
-            '  vmb help <command>\n' +
-            '  vmb      <command> --help \n\n' +
+            '  dmb help <command>\n' +
+            '  dmb      <command> --help \n\n' +
 
             'For information about command-agnostic parameters:\n' +
-            '  vmb help\n\n' +
+            '  dmb help\n\n' +
 
-            `Vermintide Mod Builder v${version}`
+            `Darktide Mod Builder v${version}`
         );
     }
 
